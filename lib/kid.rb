eb8887bcccf618948eba68c652e28a0d79081b33
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-19 04:58:12 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,8 +3,8 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid 
-  include FancyDance::InstanceMethods   # include Dance
   extend FancyDance::ClassMethods      # extend MetaDancing
+  include FancyDance::InstanceMethods   # include Dance
   
   attr_accessor :name 
   
